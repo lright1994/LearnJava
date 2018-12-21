f41d1924d720ee21c0d0763988ceadc12a47adc2
lright <lright1994@gmail.com>
2018-12-21 16:59:14 +0800

    feat(): update the singleton design pattern

--- learnDP/src/main/java/buaa/lright/Singleton.java
+++ learnDP/src/main/java/buaa/lright/Singleton.java
@@ -1,9 +1,24 @@
 package buaa.lright;
 
-public enum Singleton{
+enum Singleton{
     INSTANCE;
+    private Singleton(){};
     public void singleMethod(){
-        System.out.println("singleton");
+        System.out.println("build singleton by enum");
+    }
+}
+
+class SingleSIC{
+    private SingleSIC(){};
+
+    private static final class SingleBuild{
+        private static SingleSIC instance = new SingleSIC();
+    }
+    public static SingleSIC getSingleSIC(){
+        return SingleBuild.instance;
+    }
+    public void singleMethod(){
+        System.out.println("build singleton by inner static class");
     }
 }
 
@@ -12,6 +27,11 @@ class client{
         Singleton s1 = Singleton.INSTANCE;
         Singleton s2 = Singleton.INSTANCE;
         s1.singleMethod();
-        System.out.println("s1==s2?"+(s1==s2));
+        System.out.println("s1==s2?: "+(s1==s2));
+
+        SingleSIC sc1 = SingleSIC.getSingleSIC();
+        SingleSIC sc2 = SingleSIC.getSingleSIC();
+        sc1.singleMethod();
+        System.out.println("sc1==sc2?: "+(sc1==sc2));
     }
 }
